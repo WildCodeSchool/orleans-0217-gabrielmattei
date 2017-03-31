e1dd7c46eec936f20e11e6dfc044928855ffcb79
HageraN <hagii.neche@gmail.com>
2017-03-31 10:09:35 +0200

    Ajout de l'encodage UTF8 à la connexion PDO

--- index.php
+++ index.php
@@ -11,16 +11,25 @@ require_once 'twigloader.php';
 
 define("DSN", "mysql:host=localhost;dbname=gabrielDB");
 define("USER", "root");
-define("PASS", "Wld");
+define("PASS", "root");
 
-$pdo = new PDO(DSN, USER, PASS);
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+try {
+
+    $pdo = new PDO(DSN, USER, PASS, $options);
+}
+catch(Exception $e){
+    die('Connexion à la base de données impossible !');
+}
+
 
 $query = "SELECT * FROM content ORDER BY year desc";
 $res = $pdo->query($query);
 $resAll = $res->fetchAll(PDO::FETCH_CLASS);
 
-
-echo $twig->render('index.html.twig',
+echo $twig->render('test.html.twig',
     array(
         'contents' => $resAll
     ));
