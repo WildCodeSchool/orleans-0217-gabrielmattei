13bea51fed8a06066276c717925110c81e3ee7a8
Tandav81 <davidtanguy@gmail.com>
2017-04-18 19:01:58 +0200

    resolution pb admin

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -11,6 +11,11 @@ class AdminController extends Controller
     public function afficheAdmin()
     {
         $contentManager = new ContentManager();
+
+        if(isset($_POST['delete'])) {
+            $contentManager->deleteAction($_POST['id']);
+        }
+
         $resultats= $contentManager->findALL();
 
         return $this->getTwig()->render('admin/indexAdmin.html.twig', array('resultats' => $resultats));
@@ -20,16 +25,27 @@ class AdminController extends Controller
     public function afficheOneAdmin($id)
     {
         $contentManager = new ContentManager();
-        $resultat= $contentManager->findOne($id);
 
         if (isset($_POST['update'])){
-            $this ->update($_POST);
-            return $this->getTwig()->render('admin/indexAdmin.html.twig');
-
+            $contentManager->update();
+            header('location:index.php?p=admin');
         }
+
+        $resultat= $contentManager->findOne($id);
 
         return $this->getTwig()->render('admin/updateAdmin.html.twig', array('resultat' => $resultat));
 
     }
 
+    public function addContent()
+    {
+        $contentManager = new ContentManager();
+
+        if(isset($_POST['insert'])) {
+            $contentManager->addContent();
+            header('location:index.php?p=admin');
+        }
+
+        return $this->getTwig()->render('admin/createAdmin.html.twig');
+    }
 }
